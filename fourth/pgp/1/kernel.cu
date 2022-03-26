3ffd6f97f642afd30efae8dfff345c5d8e61f5b5
Dmitry Korostelev <54765043+Dmitry4K@users.noreply.github.com>
2022-03-26 12:31:45 +0300

    Update kernel.cu

--- fourth/pgp/1/kernel.cu
+++ fourth/pgp/1/kernel.cu
@@ -1,61 +1,65 @@
-#include <cuda.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 #include <stdio.h>
+#include <iostream>
 
+#define CUDA_ERROR_HANDLER(err) do { if (err != cudaSuccess) { std::cout << cudaGetErrorString(err) << std::endl; exit(0);} } while (0)
+#define CEH(err) CUDA_ERROR_HANDLER(err)
 
-void cudasafe(int error, char* message, char* file, int line) {
-    if (error != cudaSuccess) {
-        fprintf(stderr, "CUDA Error: %s : %i. In %s line %d\n", message, error, file, line);
-        exit(-1);
+#define BLOCKS dim3(100,100)
+#define THREADS 1024
+#define min(a, b) a < b ? a : b
+
+__global__ void minKernel(long long n, const double* a, const double* b, double* c) {
+    long long i = blockIdx.x * blockDim.x + threadIdx.x;
+    int offset = blockDim.x * gridDim.x;
+    for (; i < n; i += offset) {
+        c[i] = min(a[i], b[i]);
     }
 }
 
+int main() {
+    long long n;
+    std::cin >> n;
+    double* a, * b, *c;
+    double* dev_a, * dev_b, * dev_c = 0;
 
-int main(int argc, char** argv) {
-    int deviceCount;
+    a = new double[n];
+    b = new double[n];
+    c = new double[n];
 
-    cudasafe(cudaGetDeviceCount(&deviceCount), "GetDeviceCount", __FILE__, __LINE__);
-
-    printf("Number of CUDA devices %d.\n", deviceCount);
-
-    for (int dev = 0; dev < deviceCount; dev++) {
-        cudaDeviceProp deviceProp;
-
-        cudasafe(cudaGetDeviceProperties(&deviceProp, dev), "Get Device Properties", __FILE__, __LINE__);
-
-        if (dev == 0) {
-            if (deviceProp.major == 9999 && deviceProp.minor == 9999) {
-                printf("No CUDA GPU has been detected\n");
-                return -1;
-            }
-            else if (deviceCount == 1) {
-                printf("There is 1 device supporting CUDA\n");
-            }
-            else {
-                printf("There are %d devices supporting CUDA\n", deviceCount);
-            }
-        }
-
-        printf("For device #%d\n", dev);
-        printf("Device name:                %s\n", deviceProp.name);
-        printf("Major revision number:      %d\n", deviceProp.major);
-        printf("Minor revision Number:      %d\n", deviceProp.minor);
-        printf("Total Global Memory:        %d\n", deviceProp.totalGlobalMem);
-        printf("Total shared mem per block: %d\n", deviceProp.sharedMemPerBlock);
-        printf("Total const mem size:       %d\n", deviceProp.totalConstMem);
-        printf("Warp size:                  %d\n", deviceProp.warpSize);
-        printf("Maximum block dimensions:   %d x %d x %d\n", deviceProp.maxThreadsDim[0], \
-            deviceProp.maxThreadsDim[1], \
-            deviceProp.maxThreadsDim[2]);
-
-        printf("Maximum grid dimensions:    %d x %d x %d\n", deviceProp.maxGridSize[0], \
-            deviceProp.maxGridSize[1], \
-            deviceProp.maxGridSize[2]);
-        printf("Clock Rate:                 %d\n", deviceProp.clockRate);
-        printf("Number of muliprocessors:   %d\n", deviceProp.multiProcessorCount);
-
+    for (long long i = 0; i < n; ++i) {
+        std::cin >> a[i];
     }
 
-    return 0;
+    for (long long i = 0; i < n; ++i) {
+        std::cin >> b[i];
+    }
 
+    CUDA_ERROR_HANDLER(cudaMalloc((void**)&dev_a, n * sizeof(double)));
+    CUDA_ERROR_HANDLER(cudaMalloc((void**)&dev_b, n * sizeof(double)));
+    CUDA_ERROR_HANDLER(cudaMalloc((void**)&dev_c, n * sizeof(double)));
 
+    CUDA_ERROR_HANDLER(cudaMemcpy(dev_a, a, n * sizeof(double), cudaMemcpyHostToDevice));
+    CUDA_ERROR_HANDLER(cudaMemcpy(dev_b, b, n * sizeof(double), cudaMemcpyHostToDevice));
+
+    minKernel <<<BLOCKS, THREADS>>> (n , dev_a, dev_b, dev_c); 
+    CUDA_ERROR_HANDLER(cudaDeviceSynchronize());
+
+    CUDA_ERROR_HANDLER(cudaMemcpy(c, dev_c, n * sizeof(double), cudaMemcpyDeviceToHost));
+    
+    std::cout << std::scientific;
+    std::cout.precision(11);
+    for (long long i = 0; i < n; ++i) {
+        std::cout << c[i] << ' ';
+    }
+
+    CUDA_ERROR_HANDLER(cudaFree(dev_c));
+    CUDA_ERROR_HANDLER(cudaFree(dev_a));
+    CUDA_ERROR_HANDLER(cudaFree(dev_b));
+    delete a;
+    delete b;
+    delete c;
+    
+    exit(0);
 }
